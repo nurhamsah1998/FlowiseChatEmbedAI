d8a60b5a85976e85b90461ce5d344bf15e700b26
Nurhamsah <nur.hamsah.cash@gmail.com>
2024-09-13 13:08:42 +0700

    8

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,7 +48,7 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered byyyyyyyyyyyyyyy'}
+          'Powered byyyyyyyyyyyyyyy'
           <a
             ref={liteBadge}
             href={props.footer?.companyLink ?? 'https://flowiseai.com'}
@@ -58,7 +58,7 @@ export const Badge = (props: Props) => {
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowiseeeeeeeeeeeeeeee'}</span>
+            'Flowiseeeeeeeeeeeeeeee'
           </a>
         </span>
       </Show>
